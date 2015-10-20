47d833bc0849de1ea215c4a05c2c19679338b8ff
Carl-Erik Kopseng <carlerik@gmail.com>
2015-10-20 02:15:48 +0200

    Oppdaterte med nye completions for azure

--- generelt-oppsett/setup.sh
+++ generelt-oppsett/setup.sh
@@ -14,15 +14,15 @@ if [ ! -e "$BASH_DIR" ]; then
   mkdir ${BASH_DIR}
 fi
 
+rm ${DEST}/.bash_completion.d
+ln -sf "$PWD"/bash_completion.d ${DEST}/.bash_completion.d
+
 ln -sf "$PWD"/profile ${DEST}/.profile
 ln -sf "$PWD"/bashrc ${DEST}/.bashrc
 ln -sf "$PWD"/gitconfig ${DEST}/.gitconfig
 ln -sf "$PWD"/gitignore_global ${DEST}/.gitignore_global
 ln -sf "$PWD"/pystartup ${DEST}/.pystartup
 ln -sf "$PWD"/tmux.conf ${DEST}/.tmux.conf
-
-rm ${DEST}/.bash_completion.d
-ln -sf "$PWD"/bash_completion.d ${DEST}/.bash_completion.d
 
 for file in "$PWD"/bash.d/*; do
   ln -sf $file ${BASH_DIR}/
