baa8bd8a4cb8cfc9d64a96d69c632f6da1ccb013
Carl-Erik Kopseng <carlerik@gmail.com>
2016-02-19 16:11:41 +0100

    Added git commands

---
+++ utils/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd "$SCRIPT_DIR" > /dev/null
+
+# Get some color codes for printing
+source ../common-setup/bash.d/color_codes.dat
+
+# millis
+make install 
+
+# scripts
+cp scripts/* $HOME/bin/
+
+# Restore current directory of user
+popd > /dev/null
