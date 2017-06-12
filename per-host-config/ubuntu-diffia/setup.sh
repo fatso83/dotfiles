e17f1c001e12b8ca2c3c587bc225d6015f264acb
Carl-Erik Kopseng <carlerik@gmail.com>
2017-06-12 16:42:44 +0200

    Adding diagnostics

--- per-host-config/ubuntu-diffia/setup.sh
+++ per-host-config/ubuntu-diffia/setup.sh
@@ -3,19 +3,23 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$SCRIPT_DIR" > /dev/null
 
+# Get some color codes
+source ../../common-setup/bash.d/colors
+
 # make /usr/local owned by me
 sudo chown -R $(whoami) /usr/local
 
-# install local apps
+# 
+echo -e $(blue Installing local apps ...)
 sudo apt install -y $(cat apps.local)
 
 # upgrade PIP
 pip install --upgrade pip
 
-# install python packages
+echo -e $(blue Installing python packages ...)
 pip install -r python.local 
 
-# install ruby packages
+echo -e $(blue Installing ruby packages ...)
 while read line; do 
     if gem list -i $line > /dev/null; then
         continue
