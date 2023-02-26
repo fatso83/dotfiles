95eb76b114e9b3a40723ee191c320a24fbd4e8dd
Carl-Erik Kopseng <carlerik@gmail.com>
2023-02-26 19:05:44 +0100

    IntelliJ IDEA launcher

--- per-host-config/mac-work/setup.sh
+++ per-host-config/mac-work/setup.sh
@@ -107,3 +107,6 @@ if [[ ! -e "$HOME/.1password" ]]; then
     #see Tip in https://developer.1password.com/docs/ssh/get-started#step-4-configure-your-ssh-or-git-client
     mkdir -p ~/.1password && ln -s ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock ~/.1password/agent.sock
 fi
+
+# IntelliJ IDEA launcher
+cp ./idea.sh ~/bin/
