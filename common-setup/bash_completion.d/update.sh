e70ecc324eab49cab95535892f52ff26a365883e
Carl-Erik Kopseng <carlerik@gmail.com>
2023-03-08 16:43:57 +0100

    Update npm completion script

--- common-setup/bash_completion.d/update.sh
+++ common-setup/bash_completion.d/update.sh
@@ -1,2 +1,5 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-curl -s -o git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -s -o "$SCRIPT_DIR/git" "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
+
+npm completion > "$SCRIPT_DIR/npm"
