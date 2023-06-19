798e902240dbdf54721d5d0abcd7ae157ce87eeb
Carl-Erik Kopseng <carlerik@gmail.com>
2023-06-19 17:08:56 +0200

    Fix update script

--- common-setup/update-completion-scripts.sh
+++ common-setup/update-completion-scripts.sh
@@ -1,7 +1,9 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+BASH_D="$SCRIPT_DIR/bash_completion.d"
+
 echo "Updating BASH completion scripts for Git ..."
-curl -s -o "$SCRIPT_DIR/git" "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
+curl -s -o "$BASH_D/git" "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
 
 echo "Updating BASH completion scripts for NPM ..."
-npm completion > "$SCRIPT_DIR/npm"
+npm completion > "$BASH_D/npm"
