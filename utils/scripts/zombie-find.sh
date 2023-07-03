56ff0ea3bb28bbd8457b69036806388fef63d090
Carl-Erik Kopseng <carlerik@gmail.com>
2023-07-03 08:49:47 +0200

    Zombie finding

--- utils/scripts/zombie-find.sh
+++ utils/scripts/zombie-find.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source $SCRIPT_DIR/cross-platform-utils.bashlib
+
+if is_mac; then
+    echo "Only works on Linux"
+    exit 1
+fi
 ps -elf --forest | grep -B5 '<[d]efunct>'
