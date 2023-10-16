1060693af83a1426f9d5a01d38b48b737513dad9
Carl-Erik Kopseng <carlerik@gmail.com>
2023-10-16 10:28:41 +0200

    Check for Program, not command

--- common-setup/update-completion-scripts.sh
+++ common-setup/update-completion-scripts.sh
@@ -9,7 +9,7 @@ source "$ROOT"/shared.lib
 echo "Updating BASH completion scripts for Git ..."
 curl -s -o "$BASH_D/git" "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
 
-if command_exists npm; then
+if which npm > /dev/null; then
     echo "Updating BASH completion scripts for NPM ..."
     npm completion > "$BASH_D/npm"
 fi
