9e06c77999538cbcbf367738bf0b630d29164d7c
Carl-Erik Kopseng <carlerik@gmail.com>
2024-01-31 17:01:08 +0100

    Remove allowed signers before linking

--- common-setup/setup.sh
+++ common-setup/setup.sh
@@ -167,6 +167,7 @@ fi
 ln -sf "$SCRIPT_DIR/tool-versions" $HOME/.tool-versions
 
 [[ ! -e ~/.ssh ]] && mkdir ~/.ssh
+rm ~/.ssh/allowed_signers
 ln ./allowed_signers ~/.ssh/
 
 h2 "Finished common setup"
