adfad0c560ddc7f756915121cdefa1248142a006
Carl-Erik Kopseng <carlerik@gmail.com>
2024-08-29 10:27:24 +0200

    Stay persistent across reboots

--- per-host-config/mac-work/enable-fingerprint-for-sudo.sh
+++ per-host-config/mac-work/enable-fingerprint-for-sudo.sh
@@ -3,6 +3,12 @@
 # Based on (buggy) script in https://gist.github.com/fraune/0831edc01fa89f46ce43b8bbc3761ac7, which
 # again comes from https://azimi.io/how-to-enable-touch-id-for-sudo-on-macbook-pro-46272ac3e2df
 
+# exit on error
+set -e
+
+# https://gist.github.com/fraune/0831edc01fa89f46ce43b8bbc3761ac7?permalink_comment_id=5171048#gistcomment-5171048
+SUDO_FILE=/etc/pam.d/sudo_local
+
 tmux_fix(){
   PAM_REATTACH_MODULE=/opt/homebrew/lib/pam/pam_reattach.so
   if ! command -v tmux > /dev/null; then
@@ -16,31 +22,38 @@ Install it using Homebrew to have Touch ID work in tmux." "$PAM_REATTACH_MODULE"
       return 1
   fi
 
-  grep -q "$PAM_REATTACH_MODULE" /etc/pam.d/sudo || sudo sed -i '' '2i\
+  grep -q "$PAM_REATTACH_MODULE" $SUDO_FILE || sudo sed -i '' '1i\
 auth       optional       /opt/homebrew/lib/pam/pam_reattach.so ignore_ssh
-' /etc/pam.d/sudo
+' $SUDO_FILE
 }
 
+
 enable_touch_id(){
-  if egrep -q 'auth[[:space:]]+sufficient[[:space:]]+pam_tid.so' /etc/pam.d/sudo; then
+  RE_PAM_TID='^auth[[:space:]]+sufficient[[:space:]]+pam_tid.so'
+  if egrep -q "$RE_PAM_TID"  "$SUDO_FILE"; then
     echo "Touch ID is enabled for sudo"
   else
+    set -x
     read -p "Touch ID is not enabled for sudo. Would you like to enable it now? [y/n]: " RESPONSE
     shopt -s extglob # enables extended globbing
   
     if [[ $RESPONSE == [yY] ]]; then
-      sudo grep -q -F 'pam_tid.so' /etc/pam.d/sudo || sudo sed -i '' '2i\
+      sudo egrep -q "$RE_PAM_TID" $SUDO_FILE || sudo sed -i '' '1i\
 auth       sufficient     pam_tid.so
-' /etc/pam.d/sudo
-      if grep -q 'auth sufficient pam_tid.so' /etc/pam.d/sudo; then
-        echo "'auth sufficient pam_tid.so' added to /etc/pam.d/sudo"
+' $SUDO_FILE
+      if egrep -q "$RE_PAM_TID" $SUDO_FILE; then
+        echo "'auth sufficient pam_tid.so' added to $SUDO_FILE"
       fi
     else
-      echo "No modifications were made to /etc/pam.d/sudo"
+      echo "No modifications were made to $SUDO_FILE"
     fi
   fi
 }
 
+if [[ ! -e "$SUDO_FILE" ]]; then
+    # Create file with one line to not make sed bork
+    echo | sudo tee $SUDO_FILE > /dev/null
+fi
 enable_touch_id
 
 # the reattach module MUST be loaded before touch id module
