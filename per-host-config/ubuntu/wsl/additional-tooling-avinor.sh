6206632508459932a8a4e728c26478448e7dcbf4
Carl-Erik Kopseng <carlerik@gmail.com>
2025-05-09 12:30:22 +0200

    micro cleanup

--- per-host-config/ubuntu/wsl/additional-tooling-avinor.sh
+++ per-host-config/ubuntu/wsl/additional-tooling-avinor.sh
@@ -1,12 +1,24 @@
+#!/bin/bash
+
+set -e
+
+downloads=""
+
 download(){
     curl --fail --progress-bar -L -o "$1" "$2"
 }
 
+cleanup(){
+    echo "Running cleanup"
+    rm $downloads
+}
+
+trap  "cleanup" "EXIT"
+
 echo "Downloading Pycharm"
-download pycharm.tgz 'https://download.jetbrains.com/python/pycharm-2025.1.1.tar.gz?_gl=1*o125dt*_gcl_au*MTA0MDEzODE5OC4xNzQ2Nzc5MTI5*FPAU*MTA0MDEzODE5OC4xNzQ2Nzc5MTI5*_ga*MTYzMjQ1ODMxNy4xNzQ2Nzc5MTI1*_ga_9J976DJZ68*czE3NDY3NzkxMjUkbzEkZzEkdDE3NDY3NzkyNjQkajYwJGwwJGgw'
+downloads="pycharm.tgz"
+download pycharm.tgz 'https://download.jetbrains.com/python/pycharm-2025.1.1.tar.gz'
 mkdir -p ~/apps/pycharm
 echo "Extracting ..."
 tar xf pycharm.tgz -C ~/apps/pycharm
 echo "Pycharm extracted"
-cd ~/apps
-
