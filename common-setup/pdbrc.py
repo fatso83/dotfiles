12dfb46a1ef5fa69243dbd74d29a36f86975e92d
Carl-Erik Kopseng <carlerik@gmail.com>
2025-06-24 14:40:56 +0200

    Limit I/O to max 5k lines

--- common-setup/pdbrc.py
+++ common-setup/pdbrc.py
@@ -10,9 +10,17 @@ readline.parse_and_bind("tab: complete")
 Pdb.complete = rlcompleter.Completer(locals()).complete
 # Enable command history
 histfile = os.path.join(os.path.expanduser("~"), ".pdb_history")
+
 try:
     readline.read_history_file(histfile)
+    h_len = readline.get_current_history_length()
 except FileNotFoundError:
-    pass
+    open(histfile, 'wb').close()
+    h_len = 0
 
-atexit.register(readline.write_history_file, histfile)
+def save(prev_h_len, histfile):
+    new_h_len = readline.get_current_history_length()
+    readline.set_history_length(5000)
+    readline.append_history_file(new_h_len - prev_h_len, histfile)
+
+atexit.register(save, h_len, histfile)
