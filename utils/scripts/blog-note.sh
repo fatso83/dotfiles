d402efce7a55f9d5f4812accf486cb304de115ef
Carl-Erik Kopseng <carlerik@gmail.com>
2025-09-11 11:17:34 +0200

    Add 'slugify' and 'blog-note' script

---
+++ utils/scripts/blog-note.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+ELEVENTY_DIR="$HOME/dev/11ty-blog"
+POSTS="${ELEVENTY_DIR}/src/site/notes"
+
+DATE=$(date +"%Y-%m-%d";)
+
+TOPIC="$*"
+SLUGIFIED_TOPIC=$(slugify $TOPIC)
+FILE="$POSTS/$DATE-$SLUGIFIED_TOPIC.md"
+
+cat > "$FILE" << EOF
+---
+date: $DATE
+tags:
+    - notes
+title: "$TOPIC"
+---
+
+my text here
+EOF
+
+"${EDITOR:-nano}" "$FILE"
+
+cd "$ELEVENTY_DIR"
+git add "$FILE"
+git commit -m "Added note on \"$TOPIC\""
+git status
+echo "Push? (y/N)"
+read YESNO
+if [[ "$YESNO" =~ [yY] ]]; then
+    git push
+fi
+
