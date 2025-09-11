dbe1752ae2f354398a3775da107d1fde60b3ebb7
Carl-Erik Kopseng <carlerik@gmail.com>
2025-09-11 11:55:29 +0200

    Get rid of 'slugify' -> 'slug'. Use default-npm-packages for ASDF

--- common-setup/setup.sh
+++ common-setup/setup.sh
@@ -3,6 +3,9 @@
 BASH_DIR="${HOME}/.bash.d"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# ensures referencing local files is simpler
+cd $SCRIPT_DIR
+
 set -e #exit on error
 [[ -n $DEBUG ]] && set -x
 
@@ -128,10 +131,25 @@ vim +PlugInstall +qall
 h2 "Vim Fugitive setup"
 vim -X -u NONE -c "helptags ~/.vim/plugged/vim-fugitive/doc" -c q
 
+# used by the nodejs asdf plugin when installing a new version
+ln -sf "$SCRIPT_DIR/default-npm-packages" ~/.default-npm-packages
+
+if command_exists node; then
+    V_IN_USE=$(node --version 2> /dev/null| sed 's/v//')
+    V_WANTED=$(sed -n -e '/nodejs/s/nodejs //p' tool-versions)
+
+    if [[ "$V_IN_USE" != "$V_WANTED" ]]; then
+        h3 'Current wanted NodeJS version not matching target. Re-shimming ...'
+        asdf install nodejs "$V_WANTED"
+        # the plugin auto-installs everything in ~/.default-npm-packages
+        asdf reshim nodejs
+    fi
+fi
+
 # Needed for Typescript support in CoC using tsserver
 ts_cmd='npm install -g typescript'
-if which npm > /dev/null 2>&1 ; then
-    which tsc > /dev/null 2>&1 || bash -c "$ts_cmd"
+if command_exists npm ; then
+    bash -c "$ts_cmd"
 else
     banner "Install NodeJS and run '$ts_cmd' to get TypeScript support in Vim"
 fi
