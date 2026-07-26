a012bb7c16782b70cf9883dc63a7f77cf00a6dc6
Carl-Erik Kopseng <carlerik@gmail.com>
2026-07-26 16:38:07 +0200

    fix syntax

--- wireguard-vpn/server/setup.sh
+++ wireguard-vpn/server/setup.sh
@@ -17,6 +17,7 @@ assert_defined SERVER_PUBLIC_KEY
 assert_defined SERVER_PRIVATE_KEY
 assert_defined SERVER_WG_IP
 assert_defined WG_SUBNET
+assert_defined EXT_IF
 
 export SERVER_PUBLIC_IP
 export SERVER_WG_PORT
@@ -24,6 +25,7 @@ export SERVER_PUBLIC_KEY
 export SERVER_PRIVATE_KEY
 export SERVER_WG_IP
 export WG_SUBNET
+export EXT_IF
 
 # Create WireGuard config from template + peers
 echo "Generating wg0.conf from template and peers..."
@@ -33,4 +35,3 @@ envsubst < wg0.conf.template > wg0.header.tmp
 cat wg0.header.tmp peers.generated.conf > /etc/wireguard/wg0.conf
 rm wg0.header.tmp
 rm peers.generated.conf
-
